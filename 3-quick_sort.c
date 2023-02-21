9e7292f06a493491ac70cdf79d3e95cc94e2fbd4
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-02-21 17:22:23 +0000

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -45,7 +45,7 @@ void quick_sort_recursion(int *array, int low, int high, int size)
 
 void quick_sort(int *array, size_t size)
 {
-    if (size < 2)
-        return;
-        quick_sort_recursion(array, 0, size - 1, size);
+	if (size < 2)
+		return;
+	quick_sort_recursion(array, 0, size - 1, size);
 }
