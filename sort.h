477e68bddfa013b9c37fb7bea0b1f055b46b5593
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-21 18:17:11 +0100

    all sorts

--- sort.h
+++ sort.h
@@ -16,6 +16,14 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+
+void insertion_sort_list(listint_t **list);
+
+void selection_sort(int *array, size_t size);
+
+void quick_sort(int *array, size_t size);
+void quick_sort_recursion(int *array, int low, int high, int size);
